a6a9d66dc7576a5246255577bbd6d554eeebe94c
Chris Dillon <squarism@gmail.com>
2017-02-28 15:47:14 -0800

    shasum was wrong because me and osx

--- whatthefi.rb
+++ whatthefi.rb
@@ -3,7 +3,7 @@ class Whatthefi < Formula
   homepage "https://github.com/squarism/whatthefi"
   url "https://github.com/squarism/whatthefi/archive/v0.1.0.zip"
   version "0.1.0"
-  sha256 "a0ccd13db63450c1f607cbe9475a40da51de0130"
+  sha256 "e199fb9c2a84f1a01fccd965419c6a0d5141930334588462be1eaf86a310dfb6"
 
   def install
     bin.install buildpath/"whatthefi"
