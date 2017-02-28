0a545d530e6b9c143f339861b3a77eb5333cbff3
Chris Dillon <squarism@gmail.com>
2017-02-28 15:42:08 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+Custom homebrew formulae from your truly.  :sunflower:
+
+## Index
+
+- whatthefi: a CLI utility to help you figure out what's up with your wifi or internet:
+  - checks your ip
+  - checks if you can hit a port going out
+  - checks if a site is down for just you or everyone
+
+
+## Usage
+
+`brew tap squarism/tap`
---
+++ whatthefi.rb
@@ -0,0 +1,16 @@
+class WhatTheFi < Formula
+  desc "CLI for answering the question: WTF is up with my Wifi?"
+  homepage "https://github.com/squarism/whatthefi"
+  url "https://github.com/squarism/whatthefi/archive/v0.1.0.zip"
+  version "0.1.0"
+  sha256 "a0ccd13db63450c1f607cbe9475a40da51de0130"
+
+  def install
+    bin.install buildpath/"whatthefi"
+  end
+
+  test do
+    system "#{bin}/whatthefi --version"
+  end
+
+end
