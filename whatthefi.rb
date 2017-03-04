fe07c2f11f3d9775134337897860de1ea951cca2
Chris Dillon <squarism@gmail.com>
2017-03-03 19:21:04 -0800

    Publish crystal 0.2.0 port

--- whatthefi.rb
+++ whatthefi.rb
@@ -1,9 +1,9 @@
 class Whatthefi < Formula
   desc "CLI for answering the question: WTF is up with my Wifi?"
   homepage "https://github.com/squarism/whatthefi"
-  url "https://github.com/squarism/whatthefi/archive/v0.1.0.zip"
-  version "0.1.0"
-  sha256 "e199fb9c2a84f1a01fccd965419c6a0d5141930334588462be1eaf86a310dfb6"
+  url "https://github.com/squarism/whatthefi/releases/download/v0.2.0/whatthefi-0.2.0.osx.zip"
+  version "0.2.0"
+  sha256 "f8bd3b4f4759b0ff1a8d78c079bd9a29c1361e8bd9a387ee868147a0d32eb60d"
 
   def install
     bin.install buildpath/"whatthefi"
